c8ae5249cf840c3e0ae62f66d6b11524fb82a1fd
Renuka Choudhary <renurunrun@gmail.com>
2022-11-23 16:48:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
+import React ,{useState} from 'react';
+import Weather from './components/Weather';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [isDark, setDark] = useState(false);
+  const toggleTheme = () =>{
+    if(isDark){
+      setDark(false);
+    }else{
+      setDark(true);
+    }
+  }
+  return(
+    <div>
+     <Weather darkMode={isDark} />
+     <br/>
+     <button style={{padding:"10px",
+     backgroundColor:"pink",
+     color:" 	#4B0082",
+     borderRadius:"10%",
+     border:"none",
+     fontWeight:"bold",
+     boxShadow:"gray 2px 2px 5px"}} onClick={toggleTheme} > DARK MODE  </button>
+     <p>Created with ❤️. Happy Coding..</p>
+     <h6>Renuka Choudhary</h6>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
---
+++ src/components/Weather.js
@@ -0,0 +1,120 @@
+
+import React, { useState } from 'react'
+import axios from 'axios'
+const Weather =(props)=> {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=35cb129d5b293b1870cf54723b58aeb1`
+  
+    const searchLocation = (event) => {
+      if (event.key === 'Enter') {
+        axios.get(url).then((response) => {
+          setData(response.data)
+          console.log(response.data)
+        })
+        setLocation('')
+      }
+    }
+    if(props.darkMode){
+     return (
+      <div style={{backgroundColor:"#97aed0"}} className="App">
+        <div >
+          <input
+            className="search"
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
+        </div>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <h1>{data.name}</h1>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
+  
+          {data.name !== undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          }
+  
+  
+  
+        </div>
+      </div>
+    );
+    
+} else {
+
+    return(
+        <div  style={{backgroundColor:" #F8F8FF"}} className="App">
+        <div >
+          <input
+            className="search"
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
+        </div>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <h1>{data.name}</h1>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
+  
+          {data.name !== undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          }
+  
+  
+  
+        </div>
+      </div>
+
+    );
+    }
+    }
+    
+
+export default Weather;
